edd36b5ba7580a3ac94537924441e7dccb6b0ea9
Frad Lee <fradser@gmail.com>
2018-01-31 18:50:43 +0800

    Optimize the code

--- scripts/single-address-allocation.js
+++ scripts/single-address-allocation.js
@@ -8,7 +8,7 @@ var Tx = require('ethereumjs-tx');
 const web3 = new Web3(Web3.givenProvider || "https://ropsten.infura.io/" + process.env["INFURA_KEY"])
 // Create an async function so I can use the "await" keyword to wait for things to finish
 const main = async () => {
-    // This code was written and tested using web3 version 1.0.0-beta.28
+    // This code was written and tested using web3 version 1.0.0-beta.29
     console.log(`web3 version: ${web3.version}`)
     // Who holds the token now?
     var myAddress = "0x644dE7C3f6425D8920332FC6C97Fbf7e04445C74";
@@ -243,26 +243,30 @@ const main = async () => {
     var balance = await contract.methods.balanceOf(myAddress).call();
     console.log(`Balance before send: ${balance}`);
     // I chose gas price and gas limit based on what ethereum wallet was recommending for a similar transaction. You may need to change the gas price!
+    // Use Gwei for the unit of gas price
+    var gasPriceGwei = 60;
+    var gasLimit = 3000000;
     var rawTransaction = {
         "from": myAddress,
         "nonce": "0x" + count.toString(16),
-        "gasPrice": "0x003B9ACA00",
-        "gasLimit": "0x250CA",
+        "gasPrice": web3.utils.toHex(gasPriceGwei * 1e9),
+        "gasLimit": web3.utils.toHex(gasLimit),
         "to": contractAddress,
         "value": "0x0",
         "data": contract.methods.transfer(destAddress, transferAmount).encodeABI(),
         "chainId": 0x03
     };
-    console.log(rawTransaction);
+    console.log(`Raw of Transaction: \n${JSON.stringify(rawTransaction, null, '\t')}`);
     // The private key for myAddress in .env
     var privKey = new Buffer(process.env["PRIVATE_KEY"], 'hex');
     var tx = new Tx(rawTransaction);
     tx.sign(privKey);
     var serializedTx = tx.serialize();
-    // Comment out these three lines if you don't really want to send the TX right now
+    // Comment out these four lines if you don't really want to send the TX right now
     console.log(`Attempting to send signed tx:  ${serializedTx.toString('hex')}`);
     var receipt = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
-    console.log(`Receipt info:  ${JSON.stringify(receipt, null, '\t')}`);
+    // The receipt info of transaction, Uncomment for debug
+    console.log(`Receipt info: \n${JSON.stringify(receipt, null, '\t')}`);
     // The balance may not be updated yet, but let's check
     balance = await contract.methods.balanceOf(myAddress).call();
     console.log(`Balance after send: ${balance}`);
